4ed55b2b1a6699f623bba20c8dca708a2f11d1fd
Jongheon Kim <simba3447@gmail.com>
2016-11-18 19:52:21 +0900

    discuss threads

---
+++ routes/thread.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var router = express.Router()
+
+var Threads = function threads(id,title){
+	
+}
+
+router.get('/threads',function(req, res, next) {
+	res.render('discuss',threads)
+})
+router.get('/threads/:threadName', function(req, res, next) {
+  console.log(comment0)
+  var threadName = req.params.threadName
+  var comments = comments
+  res.render('thread.pug', { title: threadName, post: postName, comments: comments });
+});
+module.exports = router;
