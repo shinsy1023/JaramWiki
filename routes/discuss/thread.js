c4539aebbb6771fd60867ce6ecdefdcd9d2d899b
남현실 <hsilnam@naver.com>
2016-11-25 19:21:07 +0900

    thread.js pug, css와 연결해 페이지 만듦

--- routes/discuss/thread.js
+++ routes/discuss/thread.js
@@ -1,19 +1,33 @@
 var express = require('express')
 var router = express.Router()
 
-var Threads = function threads(id,title){
-	
-}
 
-router.get('/threads',function(req, res, next) {
-	res.render('discuss',threads)
+var threads = new Array();
+
+threads.push({ id: 1, title: "one"})
+threads.push({ id: 2, title: "o2ne"})
+threads.push({ id: 3, title: "on3e"})
+threads.push({ id: 4, title: "o4ne"})
+threads.push({ id: 5, title: "on5e"})
+
+var comments = new Array();
+
+comments.push({ id : 1, date : "20161124", content : "hello" })
+comments.push({ id : 2, date : "20161125", content : "world" })
+comments.push({ id : 3, date : "20161126", content : "!!!" })
+
+router.get('/',function(req, res, next) {
+	res.render('discuss',{threads:threads,post_title :"asdfadf"})
 })
-router.get('/threads/:threadName', function(req, res, next) {
-  console.log(comment0)
-  var threadName = req.params.threadName
-  var comments = comments
-  res.render('thread.pug', { title: threadName, post: postName, comments: comments });
-});
+
+// router.get('/:threadName', function(req, res, next) {
+//   var threadName = req.params.threadName
+//   var commentList = comments
+//   console.log(commentList)
+//   res.render('thread', { title: threadName, comments: commentList });
+// });
+
+
 module.exports = router;
 
 // posts/:post/threads/:thread
