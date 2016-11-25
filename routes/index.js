a405f5d3536aeea7d42fb1ada7c78fc5f7b78530
Jongheon Kim <simba3447@gmail.com>
2016-11-25 20:14:17 +0900

    fixed merge conflict

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-module.exports = function(app,commentDB) {
-  app.get('/', function(req, res, next) {
-    res.render('index', { title: 'Jaram Wiki' });
-  });
-}
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Jaram Wiki' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Jaram Wiki' });
+});
 
-// module.exports = router;
+module.exports = router;
