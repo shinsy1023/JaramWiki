6210c899c61d41b04df99fc3b062b7fb305bafb9
남현실 <hsilnam@naver.com>
2016-11-25 19:29:33 +0900

    no message

--- DB/post.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema;
-
-var postSchema = new Schema({
-	title : String,
-	rev: Number
-});
-
-module.exports = mongoose.model('post', postSchema);
